78c4d0a6cb49f0d2721911490adb7c0dd4d234a3
Salina Maharjan <salinamaharjan913@gmail.com>
2025-12-02 20:26:34 +0545

    lecture05

---
+++ Lecture5.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Lecture5 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Lecture5
+{
+     public static void main(String[] args){
+         int n=1;
+         while(n<=10)
+         {
+             System.out.println(n);
+             n++;
+         }
+         
+}
+}
---
+++ lecture51.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class lecture51 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class lecture51
+{
+     public static void main(String[] args){
+         int n =1;
+         do
+         {
+             System.out.print(n);
+             n++;
+             
+         }while(n <= 10);
+}
+}
