4da40577ef52d767c78d4bdaf11c5bd5ae2760d1
Salina Maharjan <maharjansalina913l@gmail.com>
2025-12-24 21:21:17 +0545

    week7

---
+++ Scenarioweek7/Main.java
@@ -0,0 +1,28 @@
+package Scenarioweek7;
+
+
+
+
+public class Main
+{
+    public static void main (String[]args)
+    {
+    
+        BankAccount Customer1 = new BankAccount(0001, "shreya", 180000);
+        
+        Customer1.displayDetails();
+        Customer1.depositamt(1000);
+        Customer1.withdrawl(-200);
+        Customer1.currentbalance();
+         
+        System.out.println();
+        
+        BankAccount Customer2 = new BankAccount(0011, "sara", 8000);
+        
+        Customer2.displayDetails();
+        Customer2.depositamt(40000);
+        Customer2.withdrawl(40000);
+        Customer2.currentbalance();
+    
+    }
+}
